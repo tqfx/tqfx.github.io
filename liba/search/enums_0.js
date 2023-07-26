2e96037bbf4757bd33601b4aef407894cfb54ca1
tqfx <tqfx@users.noreply.github.com>
2023-07-26 12:33:14 +0000

    opt: remove a_alloc_reg tqfx/liba@c18ee0bc496c225d92280e571c233f99fbce11fc

--- liba/search/enums_0.js
+++ liba/search/enums_0.js
@@ -1,7 +1,6 @@
 var searchData=
 [
-  ['a_5ffuzzy_5fe_0',['a_fuzzy_e',['../d6/d0a/group__A__FUZZY.html#gac636ad0f808d1b087a58c4adee1dbe01',1,'fuzzy.h']]],
-  ['a_5fmf_5fe_1',['a_mf_e',['../dd/d37/group__A__MF.html#ga811920188df5621e7b251f9a0a99c91a',1,'mf.h']]],
-  ['a_5fpid_5fe_2',['a_pid_e',['../d8/da8/group__A__PID.html#ga37fb84f61d1cda75b2e618b527c38a7b',1,'pid.h']]],
-  ['a_5fpid_5ffuzzy_5fe_3',['a_pid_fuzzy_e',['../dc/db1/group__A__PID__FUZZY.html#ga717bd50b8de6f6169bde4d7ac073ceef',1,'fuzzy.h']]]
+  ['a_5fmf_5fe_0',['a_mf_e',['../dd/d37/group__A__MF.html#ga811920188df5621e7b251f9a0a99c91a',1,'mf.h']]],
+  ['a_5fpid_5fe_1',['a_pid_e',['../d8/da8/group__A__PID.html#ga37fb84f61d1cda75b2e618b527c38a7b',1,'pid.h']]],
+  ['a_5fpid_5ffuzzy_5fe_2',['a_pid_fuzzy_e',['../dc/db1/group__A__PID__FUZZY.html#ga717bd50b8de6f6169bde4d7ac073ceef',1,'fuzzy.h']]]
 ];
