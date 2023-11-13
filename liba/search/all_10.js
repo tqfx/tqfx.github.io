0296d3125c0360bb541e59de456b061859a68c32
tqfx <tqfx@users.noreply.github.com>
2023-11-13 13:41:13 +0000

    add: configuration files published to npm tqfx/liba@4ccf833b9f81b1bdfd4221ae6c4581d96e0c7b19

--- liba/search/all_10.js
+++ liba/search/all_10.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['que_2eh_0',['que.h',['../d9/dc9/que_8h.html',1,'']]],
   ['queue_20library_1',['basic queue library',['../df/d54/group__A__QUE.html',1,'']]],
-  ['quickjs_2',['QuickJS',['../index.html#autotoc_md22',1,'']]]
+  ['quickjs_2',['QuickJS',['../index.html#autotoc_md23',1,'']]]
 ];
--- liba/search/all_d.js
+++ liba/search/all_d.js
@@ -3,9 +3,10 @@ var searchData=
   ['neuron_20proportional_20integral_20derivative_20controller_0',['single neuron proportional integral derivative controller',['../df/df3/group__A__PID__NEURON.html',1,'']]],
   ['neuron_2eh_1',['neuron.h',['../de/d38/neuron_8h.html',1,'']]],
   ['node_2',['node',['../d4/dc0/uniona__avl__u.html#af3660eb582772f4615869e6fa81bf0e4',1,'a_avl_u::node'],['../df/dfa/uniona__rbt__u.html#ad47472a820d64839caaa260795285332',1,'a_rbt_u::node']]],
-  ['note_20frequency_20table_3',['note frequency table',['../db/ddc/group__A__NOTEFREQ.html',1,'']]],
-  ['notefreqs_2eh_4',['notefreqs.h',['../dd/d08/notefreqs_8h.html',1,'']]],
-  ['num_5fn_5',['num_n',['../d0/d16/structa__tf__s.html#a31bdff0890793e36220027058bb3083d',1,'a_tf_s']]],
-  ['num_5fp_6',['num_p',['../d0/d16/structa__tf__s.html#a508fee2b25871e36306844e0bae01d0e',1,'a_tf_s']]],
-  ['number_7',['number',['../d6/dbb/group__A__COMPLEX.html',1,'complex number'],['../d5/da3/group__A__FLOAT.html',1,'floating-point number']]]
+  ['nodejs_3',['NodeJS',['../index.html#autotoc_md21',1,'']]],
+  ['note_20frequency_20table_4',['note frequency table',['../db/ddc/group__A__NOTEFREQ.html',1,'']]],
+  ['notefreqs_2eh_5',['notefreqs.h',['../dd/d08/notefreqs_8h.html',1,'']]],
+  ['num_5fn_6',['num_n',['../d0/d16/structa__tf__s.html#a31bdff0890793e36220027058bb3083d',1,'a_tf_s']]],
+  ['num_5fp_7',['num_p',['../d0/d16/structa__tf__s.html#a508fee2b25871e36306844e0bae01d0e',1,'a_tf_s']]],
+  ['number_8',['number',['../d6/dbb/group__A__COMPLEX.html',1,'complex number'],['../d5/da3/group__A__FLOAT.html',1,'floating-point number']]]
 ];
