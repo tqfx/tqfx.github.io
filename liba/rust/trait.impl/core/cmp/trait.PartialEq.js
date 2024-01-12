e27fa8fc1ae3c0ce13424b6784046a24347ee925
tqfx <tqfx@users.noreply.github.com>
2024-01-12 02:31:57 +0000

    Define macro strtonum in test.h tqfx/liba@80c6ffe38fbfd306b77871d531f11f83f24eb3e7

---
+++ liba/rust/trait.impl/core/cmp/trait.PartialEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"liba":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"liba/version/struct.version.html\" title=\"struct liba::version::version\">version</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
