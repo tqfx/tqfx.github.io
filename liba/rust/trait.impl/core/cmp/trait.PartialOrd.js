c2dacace02ebf353fa8edb1aaf9101269a416ac4
tqfx <tqfx@users.noreply.github.com>
2025-01-23 16:52:18 +0000

    deploy: tqfx/liba@b55ec18364a8b82e9b7a9c678c2db34173b8af9d

--- liba/rust/trait.impl/core/cmp/trait.PartialOrd.js
+++ liba/rust/trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["liba",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"liba/struct.version.html\" title=\"struct liba::version\">version</a>"]]]]);
+    var implementors = Object.fromEntries([["liba",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"liba/struct.version.html\" title=\"struct liba::version\">version</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
